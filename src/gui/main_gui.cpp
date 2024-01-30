5a7ac8b32a7a295118632389a0fc5c26dd0dc454
horsicq <horsicq@gmail.com>
2024-01-30 18:41:26 +0100

    Add new file(s): 2024-01-30

---
+++ src/gui/main_gui.cpp
@@ -0,0 +1,11 @@
+#include "guimainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    GuiMainWindow w;
+    w.show();
+    return a.exec();
+}
