c6292512b561753db3c47c9797c4ce7c5b407d67
horsicq <horsicq@gmail.com>
2024-02-01 18:36:56 +0100

    Update file(s): 2024-02-01

--- build/build_msvc2019_x86.bat
+++ build/build_msvc2019_x86.bat
@@ -1,5 +1,6 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86
 cd ..
 mkdir Release
 cd Release
-cmake -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:\Qt\5.15.2\msvc2019" -G "NMake Makefiles" ..
+nmake
