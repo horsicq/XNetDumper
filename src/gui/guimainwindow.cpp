8be6e6b2d0d6462cf8785b10fd4a730f53261146
horsicq <horsicq@gmail.com>
2024-02-14 19:26:32 +0100

    Update file(s): 2024-02-14

--- src/gui/guimainwindow.cpp
+++ src/gui/guimainwindow.cpp
@@ -1,3 +1,23 @@
+/* Copyright (c) 2024 hors<horsicq@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #include "guimainwindow.h"
 #include "./ui_guimainwindow.h"
 
@@ -9,11 +29,106 @@ GuiMainWindow::GuiMainWindow(QWidget *parent)
 
     XProcess::setDebugPrivilege(true);
 
+#ifdef USE_YARA
+    XYara::initialize();
+#endif
+    setWindowTitle(XOptions::getTitle(X_APPLICATIONDISPLAYNAME, X_APPLICATIONVERSION));
+
+    g_xOptions.setName(X_OPTIONSFILE);
+
+    g_xOptions.addID(XOptions::ID_VIEW_STYLE, "Fusion");
+    g_xOptions.addID(XOptions::ID_VIEW_LANG, "System");
+    g_xOptions.addID(XOptions::ID_VIEW_STAYONTOP, false);
+
+    g_xOptions.addID(XOptions::ID_SCAN_ENGINE, "auto");
+#ifdef USE_YARA
+    g_xOptions.addID(XOptions::ID_SCAN_YARARULESPATH, "$data/yara_rules");
+#endif
+    g_xOptions.load();
+
+    g_xShortcuts.setName(X_SHORTCUTSFILE);
+    g_xShortcuts.setNative(g_xOptions.isNative(), g_xOptions.getApplicationDataPath());
+
+    g_xShortcuts.addGroup(XShortcuts::GROUPID_STRINGS);
+    g_xShortcuts.addGroup(XShortcuts::GROUPID_SIGNATURES);
+    g_xShortcuts.addGroup(XShortcuts::GROUPID_HEX);
+    g_xShortcuts.addGroup(XShortcuts::GROUPID_DISASM);
+    g_xShortcuts.addGroup(XShortcuts::GROUPID_ARCHIVE);
+    g_xShortcuts.addGroup(XShortcuts::GROUPID_SCAN);
+    g_xShortcuts.load();
+
+    ui->widgetProcesses->setGlobal(&g_xShortcuts, &g_xOptions);
+
+    connect(&g_xOptions, SIGNAL(openFile(QString)), this, SLOT(_process(QString)));
+    connect(&g_xOptions, SIGNAL(errorMessage(QString)), this, SLOT(errorMessageSlot(QString)));
+
+    createMenus();
+
+    adjustWindow();
+
     ui->widgetProcesses->reload();
 }
 
 GuiMainWindow::~GuiMainWindow()
 {
+    g_xOptions.save();
+    g_xShortcuts.save();
+
     delete ui;
+#ifdef USE_YARA
+    XYara::finalize();
+#endif
+}
+
+void GuiMainWindow::createMenus()
+{
+    QMenu *pMenuFile = new QMenu(tr("File"), ui->menubar);
+    QMenu *pMenuTools = new QMenu(tr("Tools"), ui->menubar);
+    QMenu *pMenuHelp = new QMenu(tr("Help"), ui->menubar);
+
+    ui->menubar->addAction(pMenuFile->menuAction());
+    ui->menubar->addAction(pMenuTools->menuAction());
+    ui->menubar->addAction(pMenuHelp->menuAction());
+
+    QAction *pActionExit = new QAction(tr("Exit"), this);
+    QAction *pActionOptions = new QAction(tr("Options"), this);
+    QAction *pActionAbout = new QAction(tr("About"), this);
+    // QAction *pActionShortcuts = new QAction(tr("Shortcuts"), this);
+
+    pMenuFile->addAction(pActionExit);
+    // pMenuTools->addAction(pActionShortcuts);
+    pMenuTools->addAction(pActionOptions);
+    pMenuHelp->addAction(pActionAbout);
+
+    connect(pActionExit, SIGNAL(triggered()), this, SLOT(actionExitSlot()));
+    connect(pActionOptions, SIGNAL(triggered()), this, SLOT(actionOptionsSlot()));
+    connect(pActionAbout, SIGNAL(triggered()), this, SLOT(actionAboutSlot()));
+    // connect(pActionShortcuts, SIGNAL(triggered()), this, SLOT(actionShortcutsSlot()));
+}
+
+void GuiMainWindow::actionExitSlot()
+{
+    this->close();
+}
+
+void GuiMainWindow::actionOptionsSlot()
+{
+    DialogOptions dialogOptions(this, &g_xOptions, XOptions::GROUPID_FILE);
+    dialogOptions.exec();
+
+    adjustWindow();
+}
+
+void GuiMainWindow::actionAboutSlot()
+{
+    DialogAbout dialogAbout(this);
+    dialogAbout.exec();
+}
+
+void GuiMainWindow::adjustWindow()
+{
+    ui->widgetProcesses->adjustView();
+
+    g_xOptions.adjustWindow(this);
 }
 
--- src/gui/guimainwindow.h
+++ src/gui/guimainwindow.h
@@ -1,8 +1,36 @@
+/* Copyright (c) 2024 hors<horsicq@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #ifndef GUIMAINWINDOW_H
 #define GUIMAINWINDOW_H
 
 #include <QMainWindow>
+#include "../global.h"
+#include "dialogoptions.h"
+#include "dialogabout.h"
+#include "xoptions.h"
+#include "xshortcuts.h"
 #include "xprocess.h"
+#ifdef USE_YARA
+#include "xyara.h"
+#endif
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class GuiMainWindow; }
@@ -16,7 +44,17 @@ public:
     GuiMainWindow(QWidget *parent = nullptr);
     ~GuiMainWindow();
 
+private slots:
+    void createMenus();
+    void actionExitSlot();
+    // void actionShortcutsSlot();
+    void actionOptionsSlot();
+    void actionAboutSlot();
+    void adjustWindow();
+
 private:
     Ui::GuiMainWindow *ui;
+    XOptions g_xOptions;
+    XShortcuts g_xShortcuts;
 };
 #endif // GUIMAINWINDOW_H
